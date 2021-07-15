f751e660cb97417c59bb13e346b9d8719fd15973
viniciussoares7 <vinicius-a-soares@outlook.com>
2021-07-15 10:48:55 -0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import MainGrid from './src/components/MainGrid'
+import MainGrid from './src/components/mainGrid'
 import Box from './src/components/Box'
 import { AlurakutMenu, OrkutNostalgicIconSet } from './src/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from './src/components/ProfileRelations'
